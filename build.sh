0584379372d36059427076e2fedff46b644bf64b
Bryan Matsuo <bryan.matsuo@gmail.com>
2015-02-01 18:37:41 -0800

    check for godep when building

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+if [ -z $(which godep) ]; then
+    echo >&2 "install godep before building"
+    echo >&2
+    echo >&2 "    go get github.com/tools/godep"
+    echo >&2
+    exit 1
+fi
+
 godep go build -o bin/snuggied ./cmd/snuggied
 godep go build -o bin/snuggier ./cmd/snuggier
